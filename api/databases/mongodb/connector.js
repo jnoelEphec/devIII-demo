b27a1ea2169e87ac5688b53aa6099f8a01ece22a
jonathannoel <jonathan.noel1@gmail.com>
2024-03-10 23:34:11 +0100

    API Updates, routes users and kanban

---
+++ api/databases/mongodb/connector.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const connectDatabase = async () => {
+  try {
+    await mongoose.connect(
+      "mongodb://root:example@localhost:27017/mydatabase",
+      {
+        authSource: "admin",
+      }
+    );
+    console.log("MongoDB Connected");
+  } catch (error) {
+    console.error(`MongoDB connection error: ${error}`);
+    setTimeout(connectDatabase, 2000); // Réessayer après 2 secondes en cas d'échec
+  }
+};
+
+mongoose.connection.on("connected", () => {
+  console.log("Mongoose connected to DB.");
+});
+
+mongoose.connection.on("error", (error) => {
+  console.log(`Mongoose connection error: ${error}`);
+  mongoose.disconnect();
+});
+
+mongoose.connection.on("disconnected", () => {
+  console.log("Mongoose disconnected from DB.");
+  setTimeout(connectDatabase, 2000); // Réessayer après 2 secondes en cas de déconnexion
+});
+
+process.on("SIGINT", () => {
+  mongoose.connection.close(() => {
+    console.log("Mongoose disconnected due to app termination.");
+    process.exit(0);
+  });
+});
+
+module.exports = connectDatabase;
---
+++ api/databases/mongodb/entities/kanbanCard.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const kanbanCardSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: false,
+    },
+    status: {
+      type: String,
+      required: true,
+      enum: ["To Do", "In Progress", "Done"],
+      default: "To Do",
+    },
+    priority: {
+      type: Number,
+      required: false,
+    },
+    assignedTo: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: false,
+    },
+  },
+  { timestamps: true }
+);
+
+const KanbanCard = mongoose.model("KanbanCard", kanbanCardSchema);
+
+module.exports = KanbanCard;
