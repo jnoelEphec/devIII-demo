9e71698bbaa4d75c0cd4f7781eda8ae36af0992c
jnoelEphec <j.noel@ephec.be>
2025-02-01 12:00:57 +0100

    Fix unknown path to /table

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -2,7 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { KanbanBoardComponent } from './kanban/components/kanban-board/kanban-board.component';
 
-const routes: Routes = [{ path: 'table', component: KanbanBoardComponent }];
+const routes: Routes = [
+  { path: 'table', component: KanbanBoardComponent },
+  { path: '**', redirectTo: 'table' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
